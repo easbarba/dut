54a32b8af3e96cd07671db239558ce2433c14504
EAS Barbosa <easbarba@debian-BULLSEYE-live-builder-AMD64>
2022-09-11 13:44:20 -0300

    java: add options to,from,pretend,overwrite,create

--- Main.java
+++ Main.java
@@ -3,24 +3,42 @@
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
+import java.io.File;
 
-@Command(name = "Dot", mixinStandardHelpOptions = true, version = "Dot 0.1", description = "Main made with jbang")
+@Command(
+    name = "Dot", mixinStandardHelpOptions = true, version = "Dot 0.1",
+    description =
+        "create symbolic links of a folder mirroring its tree structure into $HOME or custom folder")
 class Main implements Callable<Integer> {
 
-    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
-    private String greeting;
+  @Option(names = { "-o", "--overwrite" }, description = "overwrite existent links")
+  private boolean overwrite;
 
-    public static void main(String... args) {
-        int exitCode = new CommandLine(new Main()).execute(args);
-        System.exit(exitCode);
-    }
+  @Option(names = { "-p", "--pretend" }, description = "demonstrate files linking")
+  private boolean pretend;
 
-    @Override
-    public Integer call() throws Exception { // your business logic goes here...
-        System.out.println("Hello " + greeting);
-        return 0;
-    }
+  @Option(names = { "-c", "--create" }, description = "create links of dotfiles")
+  private boolean create;
+
+  @Option(names = { "-f", "--from" }, paramLabel = "FOLDER", description = "source folder with all dotfiles")
+  File source;
+
+  @Option(names = { "-t", "--to" }, paramLabel = "FOLDER", description = "folder to deliver symbolic links")
+  File destination;
+
+  public static void main(String... args) {
+    int exitCode = new CommandLine(new Main()).execute(args);
+    System.exit(exitCode);
+  }
+
+  @Override
+  public Integer call() throws Exception { // your business logic goes here...
+    System.out.println("FROM: " + source);
+    System.out.println("TO: " + destination);
+    return 0;
+  }
 }
