0b37a0ef1c75ab7f6e38a23615f6b54c1b265f2c
easbarba <easbarba@outlook.com>
2022-09-20 13:46:11 -0300

    bash: use builtin getops for a simpler solution

--- main.bash
+++ main.bash
@@ -6,94 +6,108 @@ set -euo pipefail
 # DESCRIPTION: Yet another simple and opinionated dot files manager.
 # DEPENDENCIES: bash 4, getopt, find, cat, echo, grep, wc
 
+# make args an array, not a string
 # VARIABLES
-TO=""
-FROM=""
-CREATE=false
-REMOVE=false
-PRETEND=false
-OVERWRITE=false
+ARGS=()
 IGNORED=()
-INFO=false
+declare -A ACTIONS
+declare -A VALUES
 
 # CLI OPTIONS
 usage() {
     cat <<EOF
 Usage: dot [options]
-    --to TO                 destination folder to deliver links
-    --from FROM             target folder with dotfiles
-    --create                create links of dotfiles
-    --remove                remove links from target folder
-    --pretend               demonstrate files linking
-    --overwrite             overwrite existent links
+    -t TO, --to=TO              destination folder to deliver links
+    -f FROM, --from=FROM        target folder with dotfiles
+    -c, --create                create links of dotfiles
+    -r, --remove                remove links from target folder
+    -p, --pretend               demonstrate files linking
+    -o, --overwrite             overwrite existent links
 EOF
     exit 0
 }
 
-print_info() {
-    cat <<EOF
- -- general information --
-to:         $TO
-from:       $FROM
-create:     $CREATE
-remove:     $REMOVE
-pretend:    $PRETEND
-overwrite:  $OVERWRITE
-ignored:    ${IGNORED[@]}
-EOF
-    exit 0
-}
 
-SHORT=c,r,p,o,i,h,t:,f:
-LONG=create,remove,pretend,overwrite,help,info,to:,from:
-eval set -- "$(getopt -n dot --options $SHORT --longoptions $LONG -- "$@")"
+# replace long arguments
+for arg; do
+    case "$arg" in
+        --help)           ARGS+=(-h) ;;
+        --from)           ARGS+=(-f) ;;
+        --to)             ARGS+=(-t) ;;
+        --create)         ARGS+=(-c) ;;
+        --remove)         ARGS+=(-r) ;;
+        --pretend)        ARGS+=(-p) ;;
+        --overwrite)      ARGS+=(-o) ;;
+        --info)           ARGS+=(-i) ;;
+        *)                ARGS+=("$arg") ;;
+    esac
+done
 
 # No arguments provided
 [[ $# -eq 0 ]] && usage
 
-while true; do # keep on till there is no more arguments
-    case "$1" in
-        -t | --to)
-            _to="$2"
-            TO=${_to%%/} # remove trailing slash
-            shift 2
-            ;;
-        -f | --from)
-            _from="$2"
-            FROM=${_from%%/} # remove trailing slash
-            shift 2
-            ;;
-        -h | --help) usage ;;
-        -c | --create)
-            CREATE=true
-            break
-            ;;
-        -r | --remove)
-            REMOVE=true
-            break
-            ;;
-        -p | --pretend)
-            PRETEND=true
-            break
-            ;;
-        -o | --overwrite)
-            OVERWRITE=true
-            break
-            ;;
-        -i | --info)
-            INFO=true
-            break
-            ;;
-        --) shift ;;
+set -- "${ARGS[@]}"
+
+while getopts "hcrpoif:t:" OPTION; do
+    case $OPTION in
+        h)  usage;;
+        f)  VALUES+=([from]="$OPTARG");;
+        t)  VALUES+=([to]="$OPTARG");;
+        c)  ACTIONS+=([create]=true);;
+        r)  ACTIONS+=([remove]=true);;
+        p)  ACTIONS+=([pretend]=true);;
+        o)  ACTIONS+=([overwrite]=true);;
+        i)  ACTIONS+=([info]=true);;
+        *) usage;;
     esac
 done
 
 # LIST OF FILES TO BE IGNORED
-IGNORED_FILE="$FROM/.dotsignore"
+IGNORED_FILE="${VALUES[from]}/.dotsignore"
 [[ -f $IGNORED_FILE ]] && readarray -t IGNORED <"$IGNORED_FILE"
 IGNORED+=(.git) # user should set it, but lets be safe!
 
+print_info() {
+    echo " -- general information --"
+
+    echo
+    echo " -- Values"
+    echo
+    for v in "${!VALUES[@]}"; do
+        echo "Values: $v: ${VALUES[$v]}"
+    done
+
+    echo
+    echo " -- Actions"
+    echo
+
+    for v in "${!ACTIONS[@]}"; do
+        echo "$v: ${ACTIONS[$v]}"
+    done
+
+    echo
+echo " -- Internals"
+echo
+    echo "ignored: ${IGNORED[@]}"
+
+    exit 1
+}
+
 # ACTIONS
-[[ $INFO ]] && print_info
+[[ ${ACTIONS[info]} ]] && print_info
+
+exit
+
+# ON GOING
+
+for f in $(find "$FROM"); do
+    FILE="${f#${FROM}/}"
+
+    IGNORED_FOUND=$(echo "${IGNORED[@]}" | grep -o "$FILE" | wc -w)
+    [[ ! $IGNORED_FOUND -gt 0 ]] && continue # echo "ignoring: $FILE"
+    echo "$FILE"
+    # [[ $FILE =~ ^.git/ ]] && continue
+done
+
 
 exit
