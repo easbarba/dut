b3511e8826e27e55c156fa06cc3426b4c7346a68
easbarba <easbarba@outlook.com>
2022-09-26 13:07:48 -0300

    php: add initial cli parser

---
+++ main.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+
+$shortopts  = "";
+$shortopts .= "f:t:";  // Required value
+$shortopts .= "v::"; // Optional value
+$shortopts .= "rpo"; // These options do not accept values
+
+$longopts  = array(
+    "required:",     // Required value
+    "optional::",    // Optional value
+    "option",        // No value
+    "opt",           // No value
+);
+
+$options = getopt($shortopts, $longopts);
+var_dump($options);
+
+?>
